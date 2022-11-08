0fc5f35f21bdd6f78163c054a954b5d58134c686
ifazzzz <shoaibahmedifaz@gmail.com>
2022-11-08 21:34:01 +0600

    service api added

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
-
+// middlewares
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('review server is running')
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wbjzicb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run(){
+    try {
+      const serviceCollection = client.db('eye-care').collection('care-services');
+
+      app.get('/services', async (req, res) => {
+         const query = {};
+         const cursor = serviceCollection.find(query);
+         const result = await cursor.toArray();
+         res.send(result)
+      })
+
+      app.get('/services/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = {_id: ObjectId(id)};
+         const service = await serviceCollection.findOne(query);
+         res.send(service);
+      })
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
